03c64e069c6c8664e471b7d209f74d3b7b2d7e54
FamikSL <63651670+FamikSL@users.noreply.github.com>
2022-03-30 20:57:12 +0300

    add sample docs for test

--- tests/test_GetAllFields.py
+++ tests/test_GetAllFields.py
@@ -15,9 +15,13 @@ class TestAssertions(unittest.TestCase):
         self.assertTrue("Couldn't find docx file. Check file path!" in str(context.exception))
     
     def test_read_merge_fields(self):
-        self.assertEqual(readAllFields(r'tests/Template(test).docx'), {'Поле_1','Поле_2'})
+        self.assertEqual(readAllFields(r'sample_docs/Template(test1).docx'), {'Поле_1','Поле_2'})
 
-         
+    def test_zero_fields(self):
+        with self.assertRaises(ValueError) as context:
+            readAllFields(r'sample_docs/Template(test2).docx')
+        self.assertTrue("Your docx file doesn\'t have any merge-fields!" in str(context.exception))
+    
 
 
 if __name__ == '__main__':
