1cf8dd9bd2d9edbc785d9299158b3a064b271575
FamikSL <63651670+FamikSL@users.noreply.github.com>
2022-03-30 20:48:08 +0300

    install pandas and openpyxl

--- tests/test_GetAllFields.py
+++ tests/test_GetAllFields.py
@@ -4,7 +4,7 @@ from GetAllFields import readAllFields
 
 class TestAssertions(unittest.TestCase):
 
-    def test_zero_path(self):
+    def test_empty_path(self):
         with self.assertRaises(FileNotFoundError) as context:
             readAllFields()
         self.assertTrue("You didn\'t select docx file. Please select docx file!" in str(context.exception))
@@ -13,7 +13,12 @@ class TestAssertions(unittest.TestCase):
         with self.assertRaises(FileNotFoundError) as context:
             readAllFields('BAD_FILE_NAME.docx')
         self.assertTrue("Couldn't find docx file. Check file path!" in str(context.exception))
+    
     def test_read_merge_fields(self):
         self.assertEqual(readAllFields(r'tests/Template(test).docx'), {'Поле_1','Поле_2'})
+
+         
+
+
 if __name__ == '__main__':
     unittest.main()
