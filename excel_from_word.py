5554556f1d45ec46cd25b647a200492c8d04c21a
FamikSL <63651670+FamikSL@users.noreply.github.com>
2022-04-04 02:03:43 +0300

    rework ui, delegate funcs to other files

---
+++ excel_from_word.py
@@ -0,0 +1,4 @@
+#path = r"C:\Users\vguzaerov\Documents\GitHub\easydocs\easydocs\sample_docs\Template.docx"
+#documentFields = readAllFields('sample_docs\Template.docx')
+#df = pd.DataFrame([[1]* len(list(documentFields))],columns=list(documentFields))
+#df.to_excel("sample_docs/output.xlsx")  
